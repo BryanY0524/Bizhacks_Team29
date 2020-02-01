a849e8f96aca5e09b3e97a7626ad7b6736ea26c0
Josh W <jtwong10@gmail.com>
2020-02-01 12:26:51 -0800

    big changes for bobby

--- bizhacks-angular-frontend/src/app/app.tvquestion.ts
+++ bizhacks-angular-frontend/src/app/app.tvquestion.ts
@@ -3,26 +3,25 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { ApiService } from "./ApiService";
 
 @Component({
-    selector: "tv-q",
-    templateUrl: "TVQuestion.html"
+  selector: "tv-q",
+  templateUrl: "TVQuestion.html"
 })
 export class TVQuestionComponent {
-    public localsite = "http://localhost:4200/";
-    counter = 0;
+  public localsite = "http://localhost:4200/";
+  counter = 0;
 
-    timer = setInterval(this.next, 1000)
+  timer = setInterval(this.next, 1000);
 
-    next() {
-        this.counter += 1;
-        if (this.counter == 8) {
-            window.location.href = this.localsite + "pricematch"
-        }
+  next() {
+    this.counter += 1;
+    if (this.counter == 8) {
+      window.location.href = this.localsite + "products";
     }
+  }
 
-    back() {
-        if (this.counter < 2) {
-            console.log("beginning of qs")
-        } else
-            this.counter -= 1
-    }
+  back() {
+    if (this.counter < 2) {
+      console.log("beginning of qs");
+    } else this.counter -= 1;
+  }
 }
