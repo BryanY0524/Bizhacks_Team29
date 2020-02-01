cadefc3e1e9a670077a192059c1991a29d7b8a50
Josh W <44659422+jtw10@users.noreply.github.com>
2020-01-31 23:38:20 -0800

    angular setup

---
+++ bizhacks-angular-frontend/src/app/app.routing.ts
@@ -0,0 +1,15 @@
+import { AppComponent } from "./app.component";
+import { Component } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+import { ModuleWithProviders } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { PageDefault } from "./app.pagedefault";
+import { PriceMatchComponent } from "./app.pricematch-component";
+
+const appRoutes: Routes = [
+  { path: "pricematch", component: PriceMatchComponent },
+  { path: "**", component: PageDefault }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
