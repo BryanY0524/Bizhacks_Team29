3c04b12b7a5d104693c821c63e494ab48af6b000
Jim <jim.eho97@gmail.com>
2020-02-01 00:32:27 -0800

    walmart scrape complete

--- scrape.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-
-driver = webdriver.Chrome(".\\chromedriver")
-driver.get("https://www.walmart.ca/en/ip/nimbus9-cirrus-2-galaxy-a10e-black/6000200739606?rrid=richrelevance")
-
-print(driver.page_source)
-print("\nHTML content scraped")
