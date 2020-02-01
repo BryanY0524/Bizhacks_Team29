cc5a92f073d7d51b828a8b4e8cd4d41fdf20b3a2
dhruvparekh01 <dhruvparekh01@gmail.com>
2020-01-31 23:23:15 -0800

    create flask server

---
+++ Server/app.py
@@ -0,0 +1,23 @@
+from flask import (
+    Flask,
+    jsonify)
+
+# Create the application instance
+app = Flask(__name__, template_folder="templates")
+
+
+# Create a URL route in our application for "/"
+@app.route('/')
+def home():
+    """
+    This function just responds to the browser ULR
+    localhost:5000/
+
+    :return:        the rendered template 'home.html'
+    """
+    return jsonify(name='Hello')
+
+
+# If we're running in stand alone mode, run the application
+if __name__ == '__main__':
+    app.run(debug=True)
