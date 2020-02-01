f56b0b6fe394eba66b533f6888756d5460e60102
Josh W <jtwong10@gmail.com>
2020-02-01 00:46:04 -0800

    api service

--- bizhacks-angular-frontend/src/app/app.pricematch-component.ts
+++ bizhacks-angular-frontend/src/app/app.pricematch-component.ts
@@ -1,8 +1,38 @@
 import { Component } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { ApiService } from "./ApiService";
 
 @Component({
   selector: "app-root",
   templateUrl: "pricematch.html"
 })
-export class PriceMatchComponent {}
+export class PriceMatchComponent {
+  productPrice = "";
+  productName = "";
+  productImageUrl = "";
+
+  _apiService: ApiService;
+  public site = "http://localhost:5000/";
+
+  // API SERVICE
+  constructor(private http: HttpClient) {
+    // Pass in http module and pointer to AppComponent.
+    this._apiService = new ApiService(http, this);
+    this.getProductData();
+  }
+
+  // GETTING PRODUCT DATA
+  getProductData() {
+    this._apiService.getData("", this.productDataCallback);
+  }
+  productDataCallback(result, _this) {
+    if (result.errorMessage == "") {
+      _this.productImageUrl = result.img_url;
+      _this.productName = result.name;
+      _this.productPrice = result.price;
+      console.log(result);
+    } else {
+      alert("Unable to get data");
+    }
+  }
+}
--- bizhacks-angular-frontend/src/app/app.routing.ts
+++ bizhacks-angular-frontend/src/app/app.routing.ts
@@ -4,6 +4,9 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 import { ModuleWithProviders } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+
+import { ApiService } from "./ApiService";
+
 import { PageDefault } from "./app.pagedefault";
 import { PriceMatchComponent } from "./app.pricematch-component";
 
