ac906a17ad55f8eec3824ee1abe6e62a8a6b5eb1
dhruvparekh01 <dhruvparekh01@gmail.com>
2020-01-31 23:38:02 -0800

    Update app.py

--- Server/app.py
+++ Server/app.py
@@ -2,6 +2,7 @@ from flask import (
     Flask,
     jsonify)
 
+
 # Create the application instance
 app = Flask(__name__, template_folder="templates")
 
@@ -15,7 +16,7 @@ def home():
 
     :return:        the rendered template 'home.html'
     """
-    return jsonify(name='Hello')
+    return jsonify(name='HP Pavilion 15.6" Gaming Laptop (Intel Core i5-9300H/512GB SSD/16GB RAM/GeForce GTX 1650)', price=1399.99, img_url = 'https://images-na.ssl-images-amazon.com/images/I/810gynDZHzL._AC_SX466_.jpg')
 
 
 # If we're running in stand alone mode, run the application
